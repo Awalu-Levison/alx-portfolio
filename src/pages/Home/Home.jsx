712e8012023696b61637d086a91218ea4b366941
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-24 06:38:47 +0200

    Add home component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import Explore from '../../components/Explore/Explore'
+import AboutUs from '../../components/AboutUs/AboutUs'
+import Footer from '../../components/Footer/Footer'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Explore />
+      <AboutUs />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
