a4fe7163e3cf7603fa1daa264977bf549fe9bfdb
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-24 06:16:20 +0200

    Manage assets of the project

---
+++ src/assets/assets.js
@@ -0,0 +1,99 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import logo from './logo.png'
+import delete_icon from './delete_icon.png'
+import user_icon from './user_icon.png'
+import header_img from './header_img.png'
+import header1 from './header1.png'
+import headset1 from './headset1.JPG'
+import p2 from './p2.png'
+import afro_icon from './afro_icon.png'
+import pop2 from './pop2.png'
+import aff from './aff.png'
+import ppp from './ppp.png'
+import malawi from './malawi.png'
+import hiphop from './hiphop.png'
+import reggae from './reggae.png'
+import jazz from './jazz.png'
+import dancehall from './dancehall.png'
+import radio from './radio.png'
+
+
+export const assets = {
+    logo,
+    delete_icon,
+    user_icon,
+    header_img,
+    header1,
+    headset1,
+    p2,
+    afro_icon,
+    pop2,
+    aff,
+    ppp,
+    malawi,
+    hiphop,
+    reggae,
+    jazz,
+    dancehall,
+    radio
+
+}
+
+export const Music_List = {
+    AfroBeats: {
+      title: 'AfroBeats',
+      image: aff,
+      description:
+        'Feel the heartbeat of Africa with Afrobeats, a genre that blends traditional African rhythms with contemporary sounds. From groovy beats to uplifting melodies, Afrobeats will get you moving to the pulse of the continent. Discover tracks that bring together vibrant energy, soulful lyrics, and danceable vibes, straight from the streets of Lagos, Accra, and beyond.',
+
+    },
+
+    HipHop: {
+      title: 'HipHop Music',
+      image: hiphop,
+      description:
+        'Step into the world of Hip-Hop, where lyrical genius meets cutting-edge beats. Explore tracks that speak truth, inspire change, and celebrate self-expression. From old-school classics to modern rap anthems, Hip-Hop is more than music—it\'s a movement that defines generations. Tune in to the raw power and rhythm of street poetry..',
+
+    },
+
+    MalawiMusic: {
+        title: 'Malawi Music',
+        image: malawi,
+        description:
+          'Experience the soul of Malawi through its rich and diverse music. Whether it\'s the traditional rhythms of Gule Wamkulu, the soothing sounds of local acoustic guitars, or the vibrant beats of modern urban tracks, Malawi Music tells stories that celebrate culture, heritage, and everyday life. Dive into the melodies that define the warm heart of Africa.',
+  
+      },
+
+      Reggae: {
+        title: 'Reggae Music',
+        image: reggae,
+        description:
+          'Let the uplifting vibes of Reggae take you on a tropical escape. Known for its infectious rhythms, socially conscious lyrics, and laid-back grooves, Reggae brings the spirit of unity and positivity to life. From the legendary sounds of Bob Marley to the fresh beats of modern artists, it\'s all about peace, love, and good vibes.',
+  
+      },
+
+      PopMusic: {
+        title: 'Pop Music',
+        image: ppp,
+        description:
+          'Discover the chart-topping hits that define Pop music, the genre of universal appeal. With catchy melodies, upbeat rhythms, and lyrics that stay with you, Pop offers something for everyone. Whether you\'re in the mood for a dance anthem or a heartfelt ballad, these tracks are sure to brighten your day and keep you singing along..',
+  
+      },
+
+      JazzMusic: {
+        title: 'Jazz Music',
+        image: jazz,
+        description:
+          'Lose yourself in the timeless elegance of Jazz, a genre that speaks the universal language of music. From smooth saxophone solos to lively improvisations, Jazz takes you on a journey through emotion and creativity. Perfect for a cozy evening or moments of introspection, these tracks set the mood for a truly soulful experience..',
+  
+      },
+
+      Dancehall: {
+        title: 'Dancehall Music',
+        image: dancehall,
+        description:
+          'Dancehall is a vibrant and energetic genre of music and dance that originated in Jamaica in the late 1970s. It is a subgenre of reggae but is characterized by faster rhythms, digital instrumentation, and a focus on catchy hooks and bass-heavy beats. Dancehall is known for its bold, rebellious themes, often exploring topics like love, celebration, social commentary, and personal empowerment. Lyrically, it features a unique style of vocal delivery called deejaying, which combines rhythmic chanting, toasting, and rapping, often performed in Jamaican Patois. Dancehall culture is heavily intertwined with fashion, expressive dance moves, and dynamic performances, making it a global cultural phenomenon that influences music, fashion, and dance worldwide. Popular artists such as Beenie Man, Sean Paul, Shabba Ranks, and Spice have played significant roles in bringing dancehall to international audiences. 🎶🔥',
+  
+      },
+  }
