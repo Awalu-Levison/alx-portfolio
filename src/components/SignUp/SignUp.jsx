7cfd7d5dac7c08bb46e9f6f91e8ab3724987e19e
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-30 07:47:10 +0200

    Added sign up component

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -7,7 +7,6 @@ import { assets } from '../../assets/assets';
 import './SignUp.css';
 
 
-
 const SignUp = ({ setShowLogin }) => {
 const [currState, setCurrState] = useState('Sign in');
 const [email, setEmail] = useState('');
@@ -27,9 +26,9 @@ e.preventDefault();
 const users = getUsers();
 const user = users.find((user) => user.email === email && user.password === password);
 if (user) {
-setCurrentUser(user);
-setShowLogin(false);
-navigate('/player');
+  setCurrentUser(user);
+  setShowLogin(false);
+  navigate('/player');
 } else {setError('Invalid email or password');}
 };
 
@@ -39,49 +38,52 @@ e.preventDefault();
 const users = getUsers();
 const userExists = users.some((user) => user.email === email);
 if (userExists) {
-setError('User already exists, Sign in');
+  setError('User already exists, Sign in');
 } else {
   const newUser = { name, email, password };
-saveUser(newUser);
-setCurrentUser(newUser);
-setShowLogin(false);
-navigate('/player');
+  saveUser(newUser);
+  setCurrentUser(newUser);
+  setShowLogin(false);
+  navigate('/player');
 }
 };
 
 
 return (
-<div className='sign-up'>
-<form className='sign-up-container' onSubmit={currState === 'Sign Up' ? handleSignUp : handleSignIn}>
-<div className='sign-up-titile'>
-<h2>{currState}</h2>
-<img onClick={() => setShowLogin(false)} src={assets.delete_icon} alt='Close' />
-</div>
-<div className='sign-up-input'>
-{currState === 'Sign in' ? (<></>) : (<input type='text' placeholder='your name' value={name}
-onChange={(e) => setName(e.target.value)} required />)}
-<input type='email' placeholder='your email' value={email} onChange={(e) => setEmail(e.target.value)} required />
-<input type='password' placeholder='your password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-</div>
+  <div className='sign-up' id="signup_id">
+
+    <form className='sign-up-container' onSubmit={currState === 'Sign Up' ? handleSignUp : handleSignIn}>
+      
+      <div className='sign-up-title'>
+      <h2>{currState}</h2>
+      <img onClick={() => setShowLogin(false)} src={assets.delete_icon} alt='Close' />
+      </div>
+
+      <div className='sign-up-input'>
+      {currState === 'Sign in' ? (<></>) : (<input type='text' placeholder='your name' value={name}
+      onChange={(e) => setName(e.target.value)} required />)}
+      <input type='email' placeholder='your email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <input type='password' placeholder='your password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+      </div>
 
 
-{error && <p className='error'>{error}</p>}
-<button type='submit'>{currState === 'Sign Up' ? 'Create Account' : 'Sign in'}</button>
-<div className='sign-up-condition'>
-<input type='checkbox' required />
-<p>By continuing I agree to the terms and conditions</p>
-</div>
+      {error && <p className='error'>{error}</p>}
+      <button type='submit'>{currState === 'Sign Up' ? 'Create Account' : 'Sign in'}</button>
+      <div className='sign-up-condition'>
+      <input type='checkbox' required />
+      <p>By continuing I agree to the terms and conditions</p>
+      </div>
 
 
-{currState === 'Sign in' ? (<p>Sign Up? <span onClick={() => setCurrState('Sign Up')}>Click here</span></p>
-) : (
-<p>Already have an account? <span onClick={() => setCurrState('Sign in')}>Sign in here</span></p>
-)}
-</form>
-</div>
+      {currState === 'Sign in' ? (<p>Sign Up? <span onClick={() => setCurrState('Sign Up')}>Click here</span></p>
+      ) : (
+      <p>Already have an account? <span onClick={() => setCurrState('Sign in')}>Sign in here</span></p>
+      )}
+
+    </form>
+
+  </div>
 );
 };
-
-
 
 export default SignUp;
