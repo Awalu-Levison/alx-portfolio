310a9c695f51fb63c188ae9ede2303541491f4b3
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-30 07:48:17 +0200

    add music list

--- src/Backend/music.js
+++ src/Backend/music.js
@@ -1,22 +1,57 @@
 const songs = [
-    { title: "malele", url: "/backend/songs/Teddy.mp3" },
-    { title: "Dumpha", url: "/backend/songs/Saint.mp3" },
-    { title: "jamu", url: "path/to/song3.mp3" }
-    ];
-    
-    
-    
-    export const shuffleSongs = (songsArray) => {
+    { 
+        title: "Malele", 
+        artist: "Teddy", 
+        album: "Zikomo", 
+        year: 2023, 
+        url: "./Teddy.mp3", 
+        cover: "./teddy_cover.png" 
+    },
+    { 
+        title: "Dumpha", 
+        artist: "Saint", 
+        album: "Love Songs", 
+        year: 2022, 
+        url: "./Saint.mp3", 
+        cover: "./saint_cover.png" 
+    },
+    { 
+        title: "Jamu", 
+        artist: "Mbosso", 
+        album: "Afro Beats", 
+        year: 2021, 
+        url: "./Mbosso.mp3", 
+        cover: "./mbosso_cover.png" 
+    },
+
+    { 
+        title: "Vula-Sekele", 
+        artist: "Harry-Cane-Ft-Eemoh", 
+        album: "Afro Beats", 
+        year: 2021, 
+        url: "./Harry-Cane-Ft-Eemoh-Vula-Sekele.mp3", 
+        cover: "./vula.png" 
+    },
+
+
+    { 
+        title: "KA JIVE", 
+        artist: "Various Artists - Ka Jive-Theme-Song-Malawian_Hit", 
+        album: "Afro Beats", 
+        year: 2018, 
+        url: "./Various-Artists-KaJive-Theme-Song-Malawian_Hit.mp3", 
+        cover: "./kajive.png" 
+    },
+];
+
+export const shuffleSongs = (songsArray) => {
     return [...songsArray].sort(() => Math.random() - 0.5);
-    };
-    
-    
-    export const filterSongs = (songsArray, query) => {
+};
+
+export const filterSongs = (songsArray, query) => {
     return songsArray.filter(song =>
-    song.title.toLowerCase().includes(query.toLowerCase())
+        song.title.toLowerCase().includes(query.toLowerCase())
     );
-    };
-    
-    
-    
-    export default songs;
+};
+
+export default songs;
